4cae27d5f9a5f082939e7fb8451235d836fa4e42
Emil Juel Jakobsen <Emja19ai@student.cbs.dk>
2020-02-16 14:13:35 +0100

    ClassUser

---
+++ bruger/ClassUser.js
@@ -0,0 +1,33 @@
+class User {
+    constructor(userId, firstName, lastName, age, address, email, password, phoneNumber) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
+
+
+//class Visitor??????
+/*
+class Visitor extends User {
+    constructor(ShoppingCartId) {
+        super()
+    }
+}
+*/
+
+
+
+//Arrayliste til forskellige bruger Id
+let userList = [];
+
+//Metode til at tilføje brugere til arraylisten
+//HUSK det skal være den nyoprettede bruger som hver gang skal tilføjes til arraylisten, ellers skal den eksisterende bruger logge ind.
+
+this.userId.push
